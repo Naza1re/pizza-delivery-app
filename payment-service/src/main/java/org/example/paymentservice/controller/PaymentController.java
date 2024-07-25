0781a2bb2c415134577a0b1d64c35ecd64a10172
Naza1re <mcarim@mail.ru>
2024-07-25 14:24:04 +0300

    initial commit for payment-service

---
+++ payment-service/src/main/java/org/example/paymentservice/controller/PaymentController.java
@@ -0,0 +1,11 @@
+package org.example.paymentservice.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api/v1/payment")
+public class PaymentController {
+
+
+}
